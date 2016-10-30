b4944308b0d277c5ea7051f4d1824dff6edc6b23
avery illson <avery@untld.co>
2016-10-30 03:29:46 -0400

    updated stylesheet and images, added bodoni 72 font

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
-try{Typekit.load();}catch(e){}
+$(document).ready(function(){
+
+  // MenuToggle function to show and hide Navigation
+  (function menuToggle() {
+    var button = $('.menu'),
+     page = $('main'),
+     menuHeight = $('nav').innerHeight();
+
+    $(button).on('click', function(el){
+      el.preventDefault();
+      (page).toggleClass('slideDown');
+
+      if ( page.hasClass('slideDown') ) {
+        $(page).css('top', menuHeight );
+      } else {
+        $(page).css('top', '0' );
+      }
+
+    });
+
+  })();
+
+});
